27583a36962e17d38f000c6e659e28550e3af373
jithe <jithe@DESKTOP-45O8IC8>
2024-08-20 15:11:23 -0500

    Add project files.

---
+++ Examples.MVC/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Examples.MVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
